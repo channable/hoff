9e9b3eb440e71a2efd3639dc3b6954c9d1e8aa82
Ruud van Asseldonk <ruud@channable.com>
2018-11-23 14:28:22 +0100

    Remove unused fields from project config

--- src/Configuration.hs
+++ src/Configuration.hs
@@ -27,10 +27,7 @@ data ProjectConfiguration = ProjectConfiguration
   {
     owner      :: Text,     -- The GitHub user or organization who owns the repo.
     repository :: Text,     -- The name of the repository.
-    branch     :: Text,     -- The branch to guard and integrate commits into.
-    testBranch :: Text,     -- The branch to force-push candidates to for testing.
     checkout   :: FilePath, -- The path to a local checkout of the repository.
-    reviewers  :: [Text],   -- List of GitHub usernames that are allowed to approve.
     stateFile  :: FilePath  -- The file where project state is stored.
   }
   deriving (Generic)
